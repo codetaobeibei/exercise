921fd001cee25932cf5bdd0c9d9a7775b6105572
codetaobeibei <1223924280@qq.com>
2016-11-21 16:11:08 +0800

    Update IntersctionofTwoArraysII.cpp

--- IntersctionofTwoArraysII.cpp
+++ IntersctionofTwoArraysII.cpp
@@ -35,3 +35,11 @@ public:
         return vec;
     }
 };
+
+/*vector<int> intersect(vector<int>& a, vector<int>& b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    a.erase(set_intersection(a.begin(), a.end(), b.begin(), b.end(), a.begin()), a.end());
+    return a;
+}
+*/
