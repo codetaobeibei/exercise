e4081d77c470f8a8af4ef456f0775dd8dbf946a1
codetaobeibei <1223924280@qq.com>
2017-01-09 16:23:39 +0800

    Update PowerOfTwo.py

--- PowerOfTwo.py
+++ PowerOfTwo.py
@@ -1,8 +1,9 @@
 '''Given an integer, write a function to determine if it is a power of two.'''
 
-class Solution {
-public:
-    bool isPowerOfTwo(int n) {
-        return n>0 && !(n&(n-1));
-    }
-};
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n>0 and n&(n-1)==0
